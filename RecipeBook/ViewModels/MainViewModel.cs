c9ef28c5c1fd67f51222ea74428ac9a3dafa8cb0
khimtithsatya <tithsatya@outlook.com>
2024-02-14 09:14:38 +0700

    Iny project

---
+++ RecipeBook/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+namespace RecipeBook.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "Hello everyone";
+
+    }
+}
