7175f4046cff76aecb323b3eb9512d31a589bf09
khimtithsatya <tithsatya@outlook.com>
2024-02-16 22:56:46 +0700

    Update

--- RecipeBook/obj/Debug/net8.0-ios/iossimulator-arm64/RecipeBook.AssemblyInfo.cs
+++ RecipeBook/obj/Debug/net8.0-ios/iossimulator-arm64/RecipeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3800bd6ec4a43dd354235b420fdf3680369cd2e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
